f0edec10f3ec04119e1e0b677a87d496733eef30
Alexander Chaykin <146128141+MNESTRASHNO@users.noreply.github.com>
2025-03-18 22:37:46 +0300

    Update app.py

--- just_dir/mini_flask_app/app.py
+++ just_dir/mini_flask_app/app.py
@@ -1,1 +1,19 @@
-#just in process
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+def get_random_cat():
+    url = "https://api.thecatapi.com/v1/images/search"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()[0]['url']
+    return None
+
+@app.route('/')
+def index():
+    cat_image = get_random_cat()
+    return render_template('index.html', cat_image=cat_image)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
